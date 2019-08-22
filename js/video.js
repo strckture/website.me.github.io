fb960d61e02e4b8408ea86ac0ba073f5cc997d87
Tobizzel <tobias_schiller@aol.de>
2019-08-22 22:18:01 +0200

    video_autoplay v3
    Fixed

--- js/video.js
+++ js/video.js
@@ -1,4 +1,24 @@
+window.addEventListener("load", () => {
+	var aktuellePosition = window.location.href;
 
-setTimeout(function(){
-  document.getElementById("video_background").play();
-}, 5000);
+	if (aktuellePosition.includes("#work")){
+		video_start();
+	}
+
+});
+
+window.addEventListener("hashchange", () => {
+
+	var aktuellePosition = window.location.href;
+
+	if (aktuellePosition.includes("#work")){
+		video_start();
+	} 
+});
+
+function video_start() {
+	setTimeout(function(){
+		document.getElementById("video_background").play();
+	},2000);
+	
+}
