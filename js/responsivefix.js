fcf8866f013c07beb5325600b098ba63fc157c1f
pascal struck <struck_pascal@web.de>
2019-09-07 03:04:11 +0200

    safari fix #1

---
+++ js/responsivefix.js
@@ -0,0 +1,16 @@
+ var height = window.innerHeight;
+    var a = setInterval(function() {
+        $(window).scrollTop(-1);
+        resize(); 
+    }, 500);
+
+    $(window).on('resize', function() {
+        resize();
+    });
+
+    var resize = function() {
+        if(window.innerHeight != height) {
+            height = window.innerHeight;
+            $('.section').css('height', height + 'px');
+        }
+    };
